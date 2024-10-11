5306d30a323c7290e33fbb9bc1a19baf3f56101d
Kimhanjin <ddang0103@naver.com>
2024-10-11 18:04:38 +0900

    Feature: 회원가입 페이지 초안 로직 완료

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,60 @@
+import { useState } from "react";
+
 export default function Login() {
+  
+const email = ref();
+const password = ref()
+  // const [enteredEmail , setEnteredEmail]  = useState('');
+  // const [enteredPassword , setEnteredPassword]  = useState('');
+  
+  const [eneterValues , setEnterValues] = useState({
+    email: '',
+    password: ''
+  })
+
+
+
+  // function handleSubmit(event){
+  //   event.preventDefault();
+  // }
+
+  // function handleEmailChange(event){
+  //   setEnteredEmail(event.target.value)
+  // }
+
+  // function handlePaswordChange(event){
+  //   setEnteredEmail(event.target.value)
+  // }
+
+  // function handleInputChange(identifier,value){
+  //     setEnterValues(preveValues => ({
+  //       ...preveValues, 
+  //       [identifier]: value
+  //     }))
+  //   }
+  
+  function handleSubmit(event){
+    event.preventDefault()
+
+    const enteredEmail = email.current.value
+    const enteredPassword = email.current.value
+  }
+
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={email}/>
         </div>
       </div>
 
